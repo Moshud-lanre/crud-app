6053d8d3b8a103074a2c3788fab242cd02c7bff1
moshud-lanre <lanremoshud@gmail.com>
2021-05-14 14:51:05 +0100

    Email

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         trim: true,
         validate(value) {
             if(!validator.isEmail(value)) {
